3af68e0419f59584d4a62df2d85f65a9949c8e1b
Serge Rodovnichenko <sergerod@gmail.com>
2013-06-17 23:57:47 +0400

    Table with ranges creation

--- install.sql
+++ install.sql
@@ -3,5 +3,15 @@ CREATE TABLE IF NOT EXISTS `PREFIX_russian_post` (
     `id_state` INT(11) NOT NULL,
     `id_post_zone` INT(11) NOT NULL,
     `active` INT(11) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id_russian_post`)
     ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `PREFIX_russian_post_exclude_range` (
+`id_russian_post_exclude_range` INT(11) NOT NULL AUTO_INCREMENT,
+`id_carrier` INT(11) NOT NULL,
+`start_zip` CHAR(6) NOT NULL,
+`end_zip` CHAR(6) NOT NULL,
+`active` INT(1) NOT NULL DEFAULT 1,
+PRIMARY KEY(`id_russian_post_exclude_range`),
+KEY `range` (`start_zip`, `end_zip`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
