0945e2215cda4a6a28c314c458fdf1f0babcd847
Serge Rodovnichenko <sergerod@gmail.com>
2013-06-18 00:06:53 +0400

    Small index fix

--- install.sql
+++ install.sql
@@ -13,5 +13,5 @@ CREATE TABLE IF NOT EXISTS `PREFIX_russian_post_exclude_range` (
 `end_zip` CHAR(6) NOT NULL,
 `active` INT(1) NOT NULL DEFAULT 1,
 PRIMARY KEY(`id_russian_post_exclude_range`),
-KEY `range` (`start_zip`, `end_zip`)
+KEY `range` (`start_zip`, `end_zip`, `active`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
