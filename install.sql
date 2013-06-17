2cac4d2e2d7dff47462a6e8058fa8a6ca640a584
Serge Rodovnichenko <sergerod@gmail.com>
2013-06-17 16:10:50 +0400

    Table definition moved to the separate file

--- install.sql
+++ install.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `_PREFIX_russian_post` (
+CREATE TABLE IF NOT EXISTS `PREFIX_russian_post` (
     `id_russian_post` INT(11) NOT NULL AUTO_INCREMENT,
     `id_state` INT(11) NOT NULL,
     `id_post_zone` INT(11) NOT NULL,
