ecceb468e5eda674e620686f6635f791be7f0062
Serge Rodovnichenko <sergerod@gmail.com>
2013-06-17 23:43:01 +0400

    Added db table altering into upgrade script

--- upgrade/Upgrade-1.2.php
+++ upgrade/Upgrade-1.2.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Changes since 1.0.6
+ *
+ *  [*] id column renamed to id_russian_post
+ */
 
 if (!defined(_PS_VERSION_))
     exit;
 
+/**
+ *
+ * @param type $module
+ * @return boolean
+ */
 function upgrade_module_1_2($module) {
+
+    $sql = 'ALTER TABLE `' . _DB_PREFIX_ . 'russian_post` CHANGE `id` `id_russian_post` INTEGER';
+
+    if (!Db::getInstance()->execute($sql))
+        return FALSE;
 
     return TRUE;
 }
